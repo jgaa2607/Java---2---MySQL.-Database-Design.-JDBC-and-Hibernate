ea452c6f85421ff7f37cae644c0cb01836ae3364
Nurislambek Zhumagulov <zumagulovnurislambek@gmail.com>
2024-05-13 10:16:35 +0600

    Connection to JDBC create CRUD operations

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,15 @@
 package org.example;
+import java.sql.*;
 
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/market",
+                "postgres", "Adil2015");
+        Statement st = connection.createStatement();
+        st.executeQuery("INSERT INTO product VALUES (7, 'Заварное', '', 150, 2);");
+        ResultSet rs = st.executeQuery("select productName from product where id=6");
+        rs.next();
+        System.out.println(rs.getString(1));
+    }
 }
